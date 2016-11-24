acb18df9108b68b9d8bc9a6a8ac4875b824bd4c0
Cloudox <cloudox@foxmail.com>
2016-11-24 19:43:15 +0800

    完成动画功能

--- ImageAnimationDemo/ViewController.m
+++ ImageAnimationDemo/ViewController.m
@@ -8,6 +8,10 @@
 
 #import "ViewController.h"
 
+//设备的宽高
+#define SCREENWIDTH       [UIScreen mainScreen].bounds.size.width
+#define SCREENHEIGHT      [UIScreen mainScreen].bounds.size.height
+
 @interface ViewController ()
 
 @end
@@ -16,7 +20,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 100, SCREENWIDTH - 100, SCREENHEIGHT - 200)];
+    [self.view addSubview:imageView];
+    
+    UIImage *image1 = [UIImage imageNamed:@"xia_off"];
+    UIImage *image2 = [UIImage imageNamed:@"xia_on"];
+    UIImage *image3 = [UIImage imageNamed:@"jinxuan_off"];
+    UIImage *image4 = [UIImage imageNamed:@"jinxuan_on"];
+    UIImage *image5 = [UIImage imageNamed:@"lan_off"];
+    UIImage *image6 = [UIImage imageNamed:@"lan_on"];
+    
+    imageView.image = [UIImage animatedImageWithImages:@[image1, image2, image3, image4, image5, image6] duration:4];
 }
 
 
